7c13c0f30a4c38f65b07cce65b4e3ec4c8ad67e4
MajorBriggs <158084320+danrcross@users.noreply.github.com>
2024-08-04 14:59:59 -0400

    update about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,16 +16,15 @@ export default function About() {
           through a bootcamp program, I am developing a number of important web
           development skills. Most notably, I am learning the art of team
           development. It is a joy to work with other developers to build
-          outstanding applications, sharing and building upon each others'
+          outstanding applications-- sharing and building upon each others'
           ideas. <br />
           <br />
           As a solo developer, I am currently building a unique music
           application, called{" "}
           <a href="https://tone-zone-1314e3af579c.herokuapp.com/3">Tone Zone</a>
-          . As music is a great passion of mine (especially music composition
-          and pedagogy), I feel motivated to use and hone my web development
-          skills by engineering a useful tool for music students and educators
-          alike.
+          . As music is a love of mine, I feel motivated to hone my web
+          development skills by creating a useful tool for music students and
+          educators alike.
         </Text>
       </Box>
     </Flex>
